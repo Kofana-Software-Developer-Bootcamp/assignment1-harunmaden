0edb1884a1b7c2693adec1af95def34c53f11b5d
Harun Maden <harunmaden34@gmail.com>
2022-02-27 15:39:32 +0300

    Validation ve Reset Butonu Eklendi

--- js/dosya.js
+++ js/dosya.js
@@ -3,28 +3,38 @@ formDOM.addEventListener('submit', formSubmit)
 let adInputDOM = document.querySelector('#isim')
 let soyadInputDom = document.querySelector('#soyisim')
 let numaraInputDom = document.querySelector('#numara')
+let emailDOM = document.querySelector('#email')
 let optionDOM = document.querySelector('#kurs')
 let haftaSonuSabahDOM = document.querySelector('#haftaSonuSabah')
 let haftaSonuOglenDOM = document.querySelector('#haftaSonuOglen')
 let haftaIciSabahDOM = document.querySelector('#haftaIciSabah')
 let haftaIciAksamDOM = document.querySelector('#haftaIciAksam')
 
+function checkForm() {
+    alert("Form temizlenecek");
+    document.querySelector("#userForm").reset();
+}
 
-
-function formSubmit(event){
+function formSubmit(event) {
     event.preventDefault()
     let haftaSonuSabahCheck = document.querySelector('#haftaSonuSabah').checked
     let haftaSonuOglenCheck = document.querySelector('#haftaSonuOglen').checked
     let haftaIciSabahCheck = document.querySelector('#haftaIciSabah').checked
     let haftaIciAksamCheck = document.querySelector('#haftaIciAksam').checked
 
+    if (adInputDOM.value == "" || soyadInputDom.value == "" || numaraInputDom.value == "" || emailDOM.value == "") {
+        alert("Lütfen tüm eksik bölümleri doldurun")
+    }
+    else {
 
 
-    let items = [adInputDOM.value, soyadInputDom.value, numaraInputDom.value, optionDOM.value, 
 
-        haftaSonuSabahDOM.name,haftaSonuSabahCheck, haftaSonuOglenDOM.name, haftaSonuOglenCheck,
+        let items = [adInputDOM.value, soyadInputDom.value, numaraInputDom.value, emailDOM.value, optionDOM.value,
+
+        haftaSonuSabahDOM.name, haftaSonuSabahCheck, haftaSonuOglenDOM.name, haftaSonuOglenCheck,
         haftaIciSabahDOM.name, haftaIciSabahCheck, haftaIciAksamDOM.name, haftaIciAksamCheck]
-        
+
         console.log(items)
         alert('Bilgileriniz kaydedildi en kısa süre içerisinde size ulaşılacaktır.')
+    }
 }
